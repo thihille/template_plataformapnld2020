6e0d95b665e004f7759b16e1cb5d13d985f3251a
= <=>
2019-06-10 13:00:47 -0300

    atualizações

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# template-pnld2020
-Plataforma PNLD 2020
+# Template Plataforma PNLD2020
+
+  - Projeto utilizando Babel e Gulp.
+  - Hack js para carregamento de subtitles off-line dos vídeos do projeto.
--- dev/_config.js
+++ dev/_config.js
@@ -1,157 +1,137 @@
-let colecao = "apoema"; //apoema, aprendendo, connections, praticando e tempo
-let disciplina = "ciencias"; //arte, ciencias, geografia, historia, matematica, portugues e ingles
-let ano_colecao = 6;
+let colecao = "connections"; //apoema, aprendendo, connections, praticando e tempo
+let disciplina = "ingles"; //arte, ciencias, geografia, historia, matematica, portugues e ingles
+let ano_colecao = 7;
 let caracterizado = true;
 
 let platform = {
     inicio: [ //Preenchimento de dados
         {
             rotulo: 'Apresentação',
-            arquivo: '00-AMC6_MPD_B1_PD_001-006'
+            arquivo: '01_CNI7_MPD_B1_PD_001_004',
         }
     ],
     primeiro_bimestre: [ //Preenchimento de dados
         {
             rotulo: 'Plano de desenvolvimento do 1º bimestre',
-            arquivo: '01-AMC6_MPD_B1_PD_002-014'
+            arquivo: '01_CNI7_MPD_B1_PD_001_004',
         },
         {
             rotulo: 'Sequência didática 1 - Diferenciando misturas homogêneas e heterogêneas',
-            arquivo: '02-AMC6_MPD_B1_SD1_015-025'
+            arquivo: '02_CNI7_MPD_B1_SD_01_005_015',
         },
         {
             rotulo: 'Sequência didática 2 - Transformações químicas',
-            arquivo: '03-AMC6_MPD_B1_SD2_026-033'
+            arquivo: '03_CNI7_MPD_B1_SD_02_016_021',
         },
         {
             rotulo: 'Sequência didática 3 - Separação de materiais',
-            arquivo: '04-AMC6_MPD_B1_SD3_034-044'
+            arquivo: '04_CNI7_MPD_B1_SD_03_022_027',
         },
         {
             rotulo: 'Proposta de acompanhamento de aprendizagem',
-            arquivo: '05-AMC6_MPD_B1_PA_045-057'
+            arquivo: '05_CNI7_MPD_B1_PA_028_037',
         },
         {
             rotulo: 'Projeto integrador: 1º Bimestre',
-            arquivo: '06-AMC6_MPD_B1_PI_058-072'
+            arquivo: '06_CNI7_MPD_B1_PI_038_042',
         },
         {
-            rotulo: 'Misturas homogêneas e misturas heterogêneas',
-            arquivo: '01_misturas_homogeneas_e_misturas_heterogeneas',
+            rotulo: 'Asking the price and making a recipe',
+            arquivo: '1_asking_the_price_and_making_a_recipe',
             video: true
-        },
-        {
-            rotulo: 'A água mineral: mistura homogênea',
-            arquivo: '02_a_agua_mineral_mistura_homogenea',
-            video: true
-        },
-        {
-            rotulo: 'Transformações químicas',
-            arquivo: '03_transformacoes_quimicas',
-            video: true
-        },
+        }
 
     ],
     segundo_bimestre: [ //Preenchimento de dados
         {
             rotulo: 'Plano de desenvolvimento do 2º bimestre',
-            arquivo: '07-AMC6_MPD_B2_PD_073-080'
+            arquivo: '07_CNI7_MPD_B2_PD_043_046',
         },
         {
             rotulo: 'Sequência didática 1: Células: unidades da vida',
-            arquivo: '08-AMC6_MPD_B2_SD1_081-090'
+            arquivo: '08_CNI7_MPD_B2_SD_04_047_058',
         },
         {
             rotulo: 'Sequência didática 2: Das células aos complexos sistemas',
-            arquivo: '09-AMC6_MPD_B2_SD2_091-098'
+            arquivo: '09_CNI7_MPD_B2_SD_05_059_067',
         },
         {
             rotulo: 'Sequência didática 3: A visão e as lentes corretivas',
-            arquivo: '10-AMC6_MPD_B2_SD3_099-107'
+            arquivo: '10_CNI7_MPD_B2_SD_06_068_076',
         },
         {
             rotulo: 'Proposta de acompanhamento de aprendizagem',
-            arquivo: '11-AMC6_MPD_B2_PA_108-123'
+            arquivo: '11_CNI7_MPD_B2_PA_077_083',
         },
         {
             rotulo: 'Projeto integrador: 2º Bimestre',
-            arquivo: '12-AMC6_MPD_B2_PI_124-141'
+            arquivo: '12_CNI7_MPD_B2_PI_084_089',
         },
         {
-            rotulo: 'A célula',
-            arquivo: '04_a_celula',
-            video: true
-        },
-        {
-            rotulo: 'Sol e sombras',
-            arquivo: '05_sol_e_sombras',
+            rotulo: 'Asking for directions',
+            arquivo: '2_asking_for_directions',
             video: true
         }
     ],
     terceiro_bimestre: [ //Preenchimento de dados
         {
             rotulo: 'Plano de desenvolvimento do 3º bimestre',
-            arquivo: '13-AMC6_MPD_B3_PD_142-149'
+            arquivo: '13_CNI7_MPD_B3_PD_090_093',
         },
         {
             rotulo: 'Sequência didática 1: Sistema nervoso',
-            arquivo: '14-AMC6_MPD_B3_SD1_150-158'
+            arquivo: '14_CNI7_MPD_B3_SD_07_094_100',
         },
         {
             rotulo: 'Sequência didática 2: Movimento',
-            arquivo: '15-AMC6_MPD_B3_SD2_159-166'
+            arquivo: '15_CNI7_MPD_B3_SD_08_101_106',
         },
         {
             rotulo: 'Sequência didática 3: Sistema nervoso e as substâncias psicoativas',
-            arquivo: '16-AMC6_MPD_B3_SD3_167-177'
+            arquivo: '16_CNI7_MPD_B3_SD_09_107_115',
         },
         {
             rotulo: 'Proposta de acompanhamento de aprendizagem',
-            arquivo: '17-AMC6_MPD_B3_PA_178-192'
+            arquivo: '17_CNI7_MPD_B3_PA_116_123',
         },
         {
             rotulo: 'Projeto integrador: 3º Bimestre',
-            arquivo: '18-AMC6_MPD_B3_PI_193-213'
+            arquivo: '18_CNI7_MPD_B3_PI_124_130',
         },
         {
-            rotulo: 'Olho e visão',
-            arquivo: '06_olho_e_visao',
+            rotulo: 'Past memories',
+            arquivo: '3_past_memories',
             video: true
         }
     ],
     quarto_bimestre: [ //Preenchimento de dados
         {
             rotulo: 'Plano de desenvolvimento do 4º bimestre',
-            arquivo: '19-AMC6_MPD_B4_PD_214-220'
+            arquivo: '19_CNI7_MPD_B4_PD_131_134',
         },
         {
             rotulo: 'Sequência didática 1: As camadas da Terra',
-            arquivo: '20-AMC6_MPD_B4_SD1_221-230'
+            arquivo: '20_CNI7_MPD_B4_SD_10_135_145',
         },
         {
             rotulo: 'Sequência didática 2: Rochas e fósseis',
-            arquivo: '21-AMC6_MPD_B4_SD2_231-238'
+            arquivo: '21_CNI7_MPD_B4_SD_11_146_154',
         },
         {
             rotulo: 'Sequência didática 3: Esfericidade da Terra',
-            arquivo: '22-AMC6_MPD_B4_SD3_239-245'
+            arquivo: '22_CNI7_MPD_B4_SD_12_155_164',
         },
         {
             rotulo: 'Proposta de acompanhamento de aprendizagem',
-            arquivo: '23-AMC6_MPD_B4_PA_246-259'
+            arquivo: '23_CNI7_MPD_B4_PA_164_173',
         },
         {
             rotulo: 'Projeto integrador: 4º Bimestre',
-            arquivo: '24-AMC6_MPD_B4_PI_260-275'
+            arquivo: '24_CNI7_MPD_B4_PI_174_179',
         },
         {
-            rotulo: 'Petróleo e plástico',
-            arquivo: '07_petroleo_e_plastico',
-            video: true
-        },
-        {
-            rotulo: 'Formação do petróleo',
-            arquivo: '08_formacao_do_petroleo',
+            rotulo: 'Describing people and combatingbullying',
+            arquivo: '4_describing_people_and_combating_bullying',
             video: true
         }
     ]
